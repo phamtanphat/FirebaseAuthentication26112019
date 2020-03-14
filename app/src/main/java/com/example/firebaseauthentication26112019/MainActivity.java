43f4191b868dc71e2ecadda81c2f8eef174fb702
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-03-14 18:17:32 +0700

    anhxa

--- app/src/main/java/com/example/firebaseauthentication26112019/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthentication26112019/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.firebaseauthentication26112019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtTk,edtMk,edtNhapphonexacthuc,edtMaxacthucdienthoai;
+    Button btnDangnhap,btnDangky,btnThongtin,btnCapnhatthongtin,btnCapnhatmatkhau,btnXacthucEmail,btnXacthucphone,btnDangnhapqaxacthuc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtTk = findViewById(R.id.edittextEmail);
+        edtMk = findViewById(R.id.edittextPassword);
+        edtNhapphonexacthuc = findViewById(R.id.edittextVerificationPhone);
+        edtMaxacthucdienthoai = findViewById(R.id.edittextPhoneVerification);
+        btnDangnhap = findViewById(R.id.buttonDangnhap);
+        btnDangky = findViewById(R.id.buttonDangky);
+        btnThongtin = findViewById(R.id.buttonThongtin);
+        btnCapnhatthongtin = findViewById(R.id.buttonCapnhat);
+        btnCapnhatmatkhau = findViewById(R.id.buttonResetpassword);
+        btnXacthucEmail = findViewById(R.id.buttonVerification);
+        btnXacthucphone = findViewById(R.id.buttonVerificationPhone);
+        btnDangnhapqaxacthuc = findViewById(R.id.buttonLoginPhoneVerification);
     }
 }
